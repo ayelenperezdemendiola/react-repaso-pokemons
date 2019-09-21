39689371646edef67c03101a62144ea9209c5c3a
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2019-09-21 19:04:55 +0200

    build folder

---
+++ docs/precache-manifest.692e6d335a27a0f15f67309d39fb1fb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472b43a450b79e6c21d9134329168a87",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3ed17b8aceb9842edb41",
+    "url": "./static/css/main.6876838f.chunk.css"
+  },
+  {
+    "revision": "3ec92fe03048ca5fc703",
+    "url": "./static/js/2.31e88a96.chunk.js"
+  },
+  {
+    "revision": "3ed17b8aceb9842edb41",
+    "url": "./static/js/main.3fdbe612.chunk.js"
+  },
+  {
+    "revision": "c381a0bf91af4bc34201",
+    "url": "./static/js/runtime-main.5d165dcf.js"
+  }
+]);
